ebca792de1fe932f602c3f47eae612dd7be5712d
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2021-02-17 17:46:16 -0800

    Create word_count.py

---
+++ word_count.py
@@ -0,0 +1,9 @@
+'''Ask the user whats on their mind and then count and print the number of words in the sentence.'''
+
+user_input_amount = len(input("What's on your mind today? ").split())
+
+print("Oh nice! You just told me about your day in {} words!".format(user_input_amount))
+
+
+
+
